7aecf8e78aa7f5f284f4547e91638a4aeb85a517
dpecoraro <dpecoraro@hotmail.com>
2020-06-17 04:35:36 -0300

    First Commit

---
+++ MongoDbApi/Models/Settings/Interface/IBookstoreDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDbApi.Models.Settings.Interface
+{
+    public interface IBookstoreDatabaseSettings
+    {
+        string BooksCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
